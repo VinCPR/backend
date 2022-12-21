a95ab00b6e52f42f3cd65161045212a02f6dcca8
Tran Quang Thanh <thanhqt2002@gmail.com>
2022-12-21 21:24:59 +0700

    implement sql query group and student_to_group and list group api

---
+++ db/query/student_to_group.sql
@@ -0,0 +1,20 @@
+-- name: CreateStudentToGroup :one
+INSERT INTO "student_to_group" (
+    academic_year_id,
+    student_id,
+    group_id
+) VALUES (
+    $1 , $2, $3
+) RETURNING *;
+
+-- name: GetStudentToGroupByAcademicYearID :many
+SELECT * FROM "student_to_group"
+WHERE academic_year_id = $1;
+
+-- name: GetStudentToGroupByStudentID :many
+SELECT * FROM "student_to_group"
+WHERE student_id = $1;
+
+-- name: GetStudentToGroupByGroupID :many
+SELECT * FROM "student_to_group"
+WHERE student_id = $1;
